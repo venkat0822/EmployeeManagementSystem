f472bf07768d2d4d3c22cf18527e403a85e76716
kanagalakshmi09 <kanagasaru09@gmail.com>
2024-02-16 12:23:15 +0530

    User Details Updates

--- src/com/system/EmployeeManagement/employeeData/AddressDetails.java
+++ src/com/system/EmployeeManagement/employeeData/AddressDetails.java
@@ -1,5 +1,42 @@
 package com.system.EmployeeManagement.employeeData;
 
 public class AddressDetails {
-
+    private int doorNo,pinCode;
+    private String street,city,state,country;
+	public int getDoorNo() {
+		return doorNo;
+	}
+	public void setDoorNo(int doorNo) {
+		this.doorNo = doorNo;
+	}
+	public int getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
 }
--- src/com/system/EmployeeManagement/employeeData/BasicDetails.java
+++ src/com/system/EmployeeManagement/employeeData/BasicDetails.java
@@ -2,4 +2,43 @@ package com.system.EmployeeManagement.employeeData;
 
 public class BasicDetails {
 
+	private String empId,empName,empCurrentCompany,empPreviousCompany;
+	private int salary,age;
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpCurrentCompany() {
+		return empCurrentCompany;
+	}
+	public void setEmpCurrentCompany(String empCurrentCompany) {
+		this.empCurrentCompany = empCurrentCompany;
+	}
+	public String getEmpPreviousCompany() {
+		return empPreviousCompany;
+	}
+	public void setEmpPreviousCompany(String empPreviousCompany) {
+		this.empPreviousCompany = empPreviousCompany;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
