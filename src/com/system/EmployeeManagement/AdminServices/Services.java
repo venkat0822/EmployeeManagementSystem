c0145cdb9f5f32c5d81c4001de2de388d2497df8
kanagalakshmi09 <kanagasaru09@gmail.com>
2024-02-16 14:49:10 +0530

    services changes

--- src/com/system/EmployeeManagement/AdminServices/Services.java
+++ src/com/system/EmployeeManagement/AdminServices/Services.java
@@ -1,13 +1,12 @@
 package com.system.EmployeeManagement.AdminServices;
 import java.util.*;
 public class Services {
-<<<<<<< HEAD
+Scanner sc=new Scanner(System.in);
 public void login()
 {
-    Scanner sc=new Scanner(System.in);
     String name,password;
     System.out.println("Enter your admin Details..");
-    Syste.out.println("enter your Admin Name");
+    System.out.println("enter your Admin Name");
     name=sc.next();
     System.out.println("Enter your Password");
     password=sc.next();
@@ -17,16 +16,53 @@ public boolean AdminAuthenticate(String name,String password)
 {
 
 }
-public void SelectRole(int role)
+public void SelectRole(String role)
 {
-    if(role==1)
+    System.out.println("Enter your choice");
+    if(role.equals(""))
     {
         System.out.println("Enter your choice made up with the Employee Details..");
-        S
+        System.out.println("1.Add Employee\n2.Update Employee\n3.View Employee\n4.View All Employee\n5.Sort Employee");
+        switch(sc.nextInt())
+        {
+            case 1:
+                AddEmployee();
+                break;
+            case 2:
+                UpdateEmployee();
+                break;
+            case 3:
+                ViewEmployee();
+                break;
+            case 4:
+                ViewAllEmployee();
+                break;
+            case 5:
+                SortEmployee();
+                break;
+            default :
+                 System.out.println("Please Enter Valid options");
+                 break;
+        }
+    }
+    else if(role.equals())
+    {
+       System.out.println("1.ViewEmployee\n2.ViewAllEmployee");
+       switch(sc.nextInt())
+       {
+        case 1:
+            ViewEmployee();
+                break;
+        case 2:
+                ViewAllEmployee();
+                break;
+        default :
+
+       }
+       else
+       {
+        System.out.println("UnAuthorized person"); 
+       }
     }
 } 
-=======
-
-    
->>>>>>> 2d74bfd1f167b3f08c63c53490cfae39bb642731
 }
--- src/com/system/EmployeeManagement/Execute.java
+++ src/com/system/EmployeeManagement/Execute.java
@@ -3,6 +3,7 @@ package com.system.EmployeeManagement;
 public class Execute {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Management System...");
 		
 		
 
