ee4ce0ab0162dd895817f89eddf92f852c72caf4
venkat0822 <venkatm0980@gmail.com>
2024-02-16 14:48:31 +0530

    changes made

--- src/com/system/EmployeeManagement/AdminServices/Services.java
+++ src/com/system/EmployeeManagement/AdminServices/Services.java
@@ -1,32 +1,25 @@
 package com.system.EmployeeManagement.AdminServices;
 import java.util.*;
-public class Services {
-<<<<<<< HEAD
-public void login()
-{
-    Scanner sc=new Scanner(System.in);
-    String name,password;
-    System.out.println("Enter your admin Details..");
-    Syste.out.println("enter your Admin Name");
-    name=sc.next();
-    System.out.println("Enter your Password");
-    password=sc.next();
-    AdminAuthenticate(name,password);
-}
-public boolean AdminAuthenticate(String name,String password)
-{
 
-}
+import com.system.EmployeeManagement.AdminData.AdminDetails;
+public class Services {
+
+    public LinkedList<AdminDetails> adList = AdminDetails.returnList();
+    public void login(){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your Admin Name");
+        String name=sc.next();
+        System.out.println("Enter your Password");
+        String password=sc.next();
+        
+    }
 public void SelectRole(int role)
 {
     if(role==1)
     {
         System.out.println("Enter your choice made up with the Employee Details..");
-        S
+
     }
 } 
-=======
-
-    
->>>>>>> 2d74bfd1f167b3f08c63c53490cfae39bb642731
 }
